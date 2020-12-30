08045d24c1de56b4acd6ac0db3bd1da5e1f7c1b9
daria <karin.peisl@gmail.com>
2020-12-30 12:34:44 +0100

    First Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code hsys.out.print("Hallo Rooooooobert! :D");
+
+        String myString = "Haaaaallo Rooooobert, Bett???";
+
+        for(int i = 0; i<100; i++) {
+            System.out.println(myString);
+        }
+
+    }
+}
