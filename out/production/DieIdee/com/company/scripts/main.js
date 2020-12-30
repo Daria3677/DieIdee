38915f154be64b9bb663f39414c28255e6374449
Daria3677 <76791586+Daria3677@users.noreply.github.com>
2020-12-30 22:01:05 +0100

    First Project

---
+++ out/production/DieIdee/com/company/scripts/main.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+
+let myName = 'Karin';
+
+function multiply(num1, num2) {
+    let result = num1 * num 2;
+    return result;
+}
